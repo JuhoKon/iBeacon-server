6555be00c13e9ef9e781f0158a005018aef27e8e
JuhoKon <kontiainen97@hotmail.fi>
2021-02-02 10:48:54 +0200

    First page result is now a SSR, rest pre-renders within the revalidate interval

--- pages/content/[groupId]/[beaconId].tsx
+++ pages/content/[groupId]/[beaconId].tsx
@@ -1,28 +1,46 @@
-import { GetStaticProps, GetStaticPropsContext } from "next";
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from "next";
 import { ReactElement } from "react";
 
 interface Context extends GetStaticPropsContext {
   query: { groupId: string; beaconId: string };
 }
-export const getServerSideProps: GetStaticProps = async (context: Context) => {
+export const getStaticPaths: any = async () => {
+  return {
+    paths: [
+      {
+        params: {
+          groupId: "1",
+          beaconId: "1",
+        },
+      },
+    ],
+    fallback: "blocking",
+  };
+};
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  // Fetch data here for the pages according to the groupID and beaconID
+  // We can create a custom Error page that will be statically available, when the content is not found
+  // through error status returned from our BE
   const res = await fetch("http://worldtimeapi.org/api/ip");
   const json = await res.json();
 
-  // Here we can fetch the data per groupId and beaconId
   return {
-    props: { json, params: context.query },
+    props: { json, params },
+
+    // Re-generate the page at most per X seconds, if a request comes in
+    revalidate: 10,
   };
 };
 
 const InfoTemplate = ({ params, json }): ReactElement => {
-  console.log(params); //groupId, beaconId, etc. everything we want to include
+  console.log(params); //groupId, beaconId
 
   return (
     <>
       <h1>To be filled with content :-)</h1>
       <p>GroupId: {params?.groupId}</p>
       <p>BeaconId: {params?.beaconId}</p>
-      <p>Current time in UNIX retrieved from API : {json.unixtime}</p>
+      <p>Current time in UNIX retrieved from API : {json?.unixtime}</p>
     </>
   );
 };

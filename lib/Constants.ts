4a3881036b17e698e4d8296d492b40f76f4bd27e
Juho Kontiainen <37773658+JuhoKon@users.noreply.github.com>
2021-04-26 16:43:44 +0300

    Update Constants.ts

--- lib/Constants.ts
+++ lib/Constants.ts
@@ -1,4 +1,4 @@
 export const LOCALIZATION = "localizations";
 export const TOURS = "tours";
 export const NOT_FOUND = "Not found";
-export const CACHE_TTL_SECONDS = 60;
+export const CACHE_TTL_SECONDS = 7889226; // 3 months
